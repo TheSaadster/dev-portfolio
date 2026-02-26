2519efe99d9a70181571288d21a6610e89e62346
Saad <saadaulakh04@gmail.com>
2026-02-26 12:14:51 -0500

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Saad | Full-Stack Developer",
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} bg-gray-950 text-white`}>
+        <Analytics />
         <Navbar />
         {children}
       </body>
